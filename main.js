0bed0cb87889d8b632590db0dc8b7e95b9573682
NguyenHoangHieu <hoanghieufro@gmail.com>
2024-01-25 15:24:40 +0700

    a

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 console.log("Hello world");
 
-function calculator() {}
+function calculator(theCalculation) {
+  return theCalculation();
+}
 
-function ConvertCToF() {}
+function ConvertCToF(degreeNumber) {
+  return calculator(() => {
+    // (0°C × 9/5) + 32 = 32°F
+    return (degreeNumber * 9) / 5 + 32;
+  });
+}
